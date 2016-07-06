6747fc3bc753a72ffc8a7060ee693a18b5b28d14
Vicky Tu <vcctu85@berkeley.edu>
2016-07-06 11:41:19 -0700

    video

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -48,7 +48,7 @@ angular.module('starter.controllers', [])
     { title: 'Military', id: 3 , image: 'img/military.jpg', occupations: ["Air Force", "Navy"]},
     { title: 'Legal/Politics', id: 4 , image: 'img/legal.jpg', occupations: ["Lawyer", "Judge"]},
     { title: 'Business', id: 5 ,  image: 'img/business.jpg', occupations: ["Sales Associate", "Youtuber"]},
-    { title: 'Agriculture', id: 6 ,  image: 'img/agriculture.png', occupations:["Farmer", "Urban gardener"]},
+    { title: 'Agriculture', id: 6 ,  image: 'img/agriculture.png', occupations:["Farmer", "Urban gardener", "https://www.youtube.com/watch?v=ZPXM1tmpjWs"]},
     { title: 'Communication', id: 7 ,  image: 'img/communication.jpeg', occupations:["Radio Host", "UN representative"]},
     { title: 'Engineering', id: 8 ,  image: 'img/engineering.jpg', occupations:["Software Engineer", "Bioengineer"]},
     { title: 'Media & Entertainment', id: 9 ,  image: 'img/media.png', occupations:["Actor", "Clown"]},
