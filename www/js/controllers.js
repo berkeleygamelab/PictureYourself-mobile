cc8cb77791dd8139251a3199968462a57712e74f
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-07-13 22:36:16 +0200

    Fixes bug in for loop.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,17 +70,21 @@ angular.module('starter.controllers', [])
     { title: 'Engineering', id: 8 ,  image: 'img/engineering.jpeg', occupations:["Software Engineer", "Bioengineer"]},
     { title: 'Media & Entertainment', id: 9 ,  image: 'img/media.png', occupations:["Actor", "Clown"]},
     { title: 'Civil Service', id: 10 ,  image: 'img/civil.png', occupations:["Social Worker", "Mayor"]},
-    { title: 'Arts & Literature', id: 11 ,  image: 'img/arts.png', occupations:[{name:"Edward Ruscha", id: 1, title: "Artist", bio: "Edward “Ed” Ruscha is an Artist working with the pop artist movement. He worked with photography, painting, printmaking, and film. He created famous paintings and other successful works based upon the pop art movement.", accomplishments: "Graduated Chouinard Art Institute; Received the Guggenheim Fellowship Award in 1971", video: "https://www.nowness.com/series/getting-there/ed-ruscha", image:"img/edwardruscha.jpg"},
-    {name: "Stephan King", id: 2, title: "Author", bio: "Stephen Edwin King is an American author/novelist who writes books based upon science fiction, horror, fantasy, etc. His famous books received multiple awards and some even ended up on the big screen.", accomplishments: "Graduated from the University of Maine; King was nominated for the primetime emmy in 1997 for his famous miniseries “The Shining” which was based on his infamous novel.", video: "http://stephenking.com/", image: "img/stephenking.jpg"}]}
+    { title: 'Arts & Literature', id: 11 ,  image: 'img/arts.png', occupations:[
+      {name:"Edward Ruscha", id: 1, title: "Artist", bio: "Edward “Ed” Ruscha is an Artist working with the pop artist movement. He worked with photography, painting, printmaking, and film. He created famous paintings and other successful works based upon the pop art movement.", accomplishments: "Graduated Chouinard Art Institute; Received the Guggenheim Fellowship Award in 1971", video: "https://www.nowness.com/series/getting-there/ed-ruscha", image:"img/edwardruscha.jpg"},
+      {name: "Stephan King", id: 2, title: "Author", bio: "Stephen Edwin King is an American author/novelist who writes books based upon science fiction, horror, fantasy, etc. His famous books received multiple awards and some even ended up on the big screen.", accomplishments: "Graduated from the University of Maine; King was nominated for the primetime emmy in 1997 for his famous miniseries “The Shining” which was based on his infamous novel.", video: "http://stephenking.com/", image: "img/stephenking.jpg"}]}
   ]
 
   for (var i = 0; i < $scope.careers.length; i++){
-    var occupations = $scope.careers[i].occupations;
+    if ($stateParams.playlistId == $scope.careers[i].id) {
+      $scope.career = $scope.careers[i];
+    }
+  }
 
-    for (var j = 0; j < occupations.length; j++) {
-      if ($stateParams.careerName == occupations[j].id) {
-        $scope.occupation = occupations[j];
-      }
+  var occupations = $scope.career.occupations;
+  for (var j = 0; j < occupations.length; j++) {
+    if ($stateParams.careerName == occupations[j].id) {
+      $scope.occupation = occupations[j];
     }
   }
 })
